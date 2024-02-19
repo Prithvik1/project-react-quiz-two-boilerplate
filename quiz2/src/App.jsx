a41395944ee7b3feb992ebbc349e45c76a969e73
Prithvi Kashyap <prithvi.kashyap1804@gmail.com>
2024-02-19 10:42:27 +0530

    updates

---
+++ quiz2/src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Quiz from "./Components/Quiz";
+import "./App.css";
+
+export default class App extends Component{
+  render(){
+    return(
+      <div class="quiz">
+        
+        <Quiz/> 
+       
+      </div>
+    );
+  };
+}
